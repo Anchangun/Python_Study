765c0e249ee6acc92f9454425886053cec567263
DESKTOP-V7DNS3G\chang <changun516@gmail.com>
2020-12-04 01:47:44 +0900

    Chapter03 공부 및 풀이

---
+++ OReilly/Introduction_Python/Chapter_03/Type_Trans/Type_Trans.py
@@ -0,0 +1,17 @@
+print("int_FUNC_TRUE :",int(True))
+print("int_FUNC_FALSE :",int(False))
+print("bool_FUNC_1 :",bool(1))
+print("bool_FUNC_0 :",bool(0))
+print("int_FUNC_98.6 :",int(98.6))
+print("int_FUNC_1.0e4 :",int(1.0e4))
+print("bool_FUNC_1.0 :",bool(1.0))
+print("bool_FUNC_0.0 :",bool(0.0))
+
+print("int_FUNC_10,2",int('10',2))
+print("int_FUNC_10,8",int('10',8))
+print("int_FUNC_10,16",int('10',16))
+print("int_FUNC_10,22",int('10',22))
+
+print("type Trans_4+7.0 :",4+7.0)
+
+print("type Trans_true+2 :",True+2)
