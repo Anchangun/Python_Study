cc1e5277c62db6695296ca98528da0f0251d4ab7
DESKTOP-V7DNS3G\chang <changun516@gmail.com>
2020-12-10 00:54:46 +0900

    문자열 사용 예제

---
+++ OReilly/Introduction_Python/Chapter_05/CreateStringInQuotationmarks.py
@@ -0,0 +1,9 @@
+print('Snap')
+print('Crackle')
+print("'hi Python'")
+print('''i'm so tired''')
+poem='''I do not like thee,DoctorFell.
+The Reason why, icannot tell'''
+print(poem)
+
+print('python',"""c""",'''c++''',"java")
